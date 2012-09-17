b2d459c2037419107fe3aa3c5d7c4cf958fff1e7
dAr2q-PC <noreply@u2l-gaming.co.de>
2012-09-17 05:43:56 +0200

    system erkennung

--- WaWPatcher-R3/Form1.vb
+++ WaWPatcher-R3/Form1.vb
@@ -3,7 +3,6 @@ Imports System
 Imports System.IO
 Imports Microsoft.Win32.Registry
 
-
 Public Class Form1
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
@@ -17,8 +16,13 @@ Public Class Form1
         'MsgBox("Das Spiel wurde gefunden", MsgBoxStyle.Information, "gefunden")
         'End If
     End Sub
+    Private Function GetCpuMode() As String
+        '32Bit IntPtr.Size = 4 [ 4 * 8] = 32
+        '64Bit IntPtr.Size = 8 [ 8 * 8] = 64
+        Return If(IntPtr.Size = 8, "64Bit System", "32Bit System")
+    End Function
 
     Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
-        MsgBox(ProductName & vbNewLine & "a Simple Tool for patching World At War to Version 1.7" & vbNewLine & "-built for testing purposes only", MsgBoxStyle.Information, "about")
+        MsgBox(ProductName & vbNewLine & "a Simple Tool for patching World At War to Version 1.7" & vbNewLine & "-built for testing purposes only" & vbNewLine & "Es wird ein " & GetCpuMode() & " verwendet", MsgBoxStyle.Information, "about")
     End Sub
 End Class
